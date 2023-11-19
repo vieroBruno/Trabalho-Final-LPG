e6c6cc8f3dd952b48af5ec116b60f192c9b10d5c
Bruno Gabriel Viero <124797174+vieroBruno@users.noreply.github.com>
2023-11-19 18:19:39 -0300

    Criação do Menu

--- TrabalhoFinalLPG.c
+++ TrabalhoFinalLPG.c
@@ -1,10 +1,44 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+/* Function prototypes */
 
-int main(int argc, char *argv[]) {
-	printf("Testando commit");
-	printf("haha");
-	return 0;
+int main() {
+    int menu = 1;
+
+    while (menu == 1) {
+        printf("Cadastro de Livros\n Escolha uma opcao:\n");
+        printf("-> (1) Inserir um novo livro\n");
+        printf("-> (2) Listar todos os livros\n");
+        printf("-> (3) Consultar um livro\n");
+        printf("-> (4) Remover um livro\n");
+        printf("-> (5) Sair\n");
+
+        int opcaomenu;
+        scanf("%d", &opcaomenu);
+
+        switch (opcaomenu) {
+            case 1:
+                
+                break;
+            case 2:
+               
+                break;
+            case 3:
+                
+                break;
+            case 4:
+                
+                break;
+            case 5:
+                menu = 0; 
+                break;
+            default:
+                printf("Opcao invalida. Tente novamente.\n");
+        }
+    }
+
+    return 0;
 }
+
+
