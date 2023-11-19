48a7ef0bd4e3018522cf391dd4b69c4b717e4fe8
Bruno Gabriel Viero <124797174+vieroBruno@users.noreply.github.com>
2023-11-19 17:43:58 -0300

    teste 2

--- TrabalhoFinalLPG.c
+++ TrabalhoFinalLPG.c
@@ -4,5 +4,7 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
+	printf("Testando commit");
+	
 	return 0;
 }
